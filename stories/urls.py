47dd44111fd7eaf9d02e3320fc17a9860521c4e1
Cyril Jean <cyril.jean@gmail.com>
2013-03-10 10:41:24 +0800

    removed date filtering

--- stories/urls.py
+++ stories/urls.py
@@ -6,17 +6,15 @@ from django.utils import timezone
 urlpatterns = patterns('',
 	url(r'^$',         
 		ListView.as_view(
-			queryset=Story.objects.filter(pub_date__lte=timezone.now).order_by('-pub_date')),
+			queryset=Story.objects.order_by('-pub_date')),
 	name='index'),
 	url(r'^json/$',         
 		ListView.as_view(
-			queryset=Story.objects.filter(pub_date__lte=timezone.now).order_by('-pub_date'),
+			queryset=Story.objects.order_by('-pub_date'),
 			template_name='stories/story_list.json'),
 	name='index'),
     # ex: /stories/5/
 	url(r'^(?P<pk>\d+)/$', 
-        DetailView.as_view(
-            queryset=Story.objects.filter(pub_date__lte=timezone.now),
-            template_name='stories/detail.html'),
+        DetailView.as_view(model=Story),
 	name='detail'),
 )
