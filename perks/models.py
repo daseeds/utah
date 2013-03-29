225db8a2dc970d0926a2691e0b91501c0cb9184c
Cyril Jean <cyril.jean@gmail.com>
2013-03-29 18:23:43 +0800

    Warning ! modified Perks model

--- perks/models.py
+++ perks/models.py
@@ -3,6 +3,14 @@ from django.db import models
 class Perk(models.Model):
 	pub_date = models.DateTimeField('date published')
 	title = models.CharField(max_length=200)
+	PERK_TYPE_CHOICES = (
+        ('no', 'Normal element'),
+        ('in', 'Information'),
+        ('al', 'Alert'),
+	)
+	type = models.CharField(max_length=2,
+							choices=PERK_TYPE_CHOICES,
+							default='no');
 	def __unicode__(self):
 		return self.title
 
